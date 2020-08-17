a3279cd48a8dc101dee028266fcd2f17fbfa187a
Kelsie Merchant <eislek02@gmail.com>
2020-08-17 18:59:28 -0500

    Update script to pass shellcheck and expand escape sequence in echo

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,7 @@ sudo apt-get -y install nginx
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 touch /data/web_static/releases/test/index.html
-echo "simple content\n" > /data/web_static/releases/test/index.html
+echo -e "simple content\n" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown --recursive ubuntu:ubuntu /data/
 sed -i 's|location / {|location /hbnb_static {\n\t\talias /data/web_static/current/\n\t}\n\n\tlocation / {|' /etc/nginx/sites-available/default
