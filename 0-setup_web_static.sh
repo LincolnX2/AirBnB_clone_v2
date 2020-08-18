531baa664b1f4a2d0204fc99a99f7063706f8536
Kelsie Merchant <eislek02@gmail.com>
2020-08-17 19:30:35 -0500

    Update script to only match uncommented lines for location in /etc/nginx/sites-available/default

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,7 @@ sudo apt-get -y install nginx
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 echo -e "simple content\n" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+ln -sf /data/web_static/releases/test /data/web_static/current
 chown --recursive ubuntu:ubuntu /data/
-sed -i 's|location / {|location /hbnb_static {\n\t\talias /data/web_static/current/\n\t}\n\n\tlocation / {|' /etc/nginx/sites-available/default
+sed -i 's|^\tlocation / {|\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n\n\tlocation / {|' /etc/nginx/sites-available/default
 service nginx restart
