7ba2a075373a6cfd608c3d19229c4a9fdc5e28ac
Kelsie Merchant <eislek02@gmail.com>
2020-09-02 00:49:29 -0500

    Update python code to not automatically execute when imported

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,15 +5,15 @@ listens on 0.0.0.0, port 5000
 routes: /: display "Hello HBNB!"
 strict_slashes=False
 """
-from flask import Flask
-app = Flask(__name__)
+if __name__ == '__main__':
+    from flask import Flask
+    app = Flask(__name__)
 
+    @app.route('/', strict_slashes=False)
+    def hello_hbnb():
+        """
+        displays "Hello HBNB!"
+        """
+        return "Hello HBNB!"
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """
-    displays "Hello HBNB!"
-    """
-    return "Hello HBNB!"
-
-app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port='5000')
