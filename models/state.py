a7d5699120588c3612f5807b628f847918b56eab
Kelsie Merchant <eislek02@gmail.com>
2020-09-02 21:08:07 -0500

    Add conditions to only set getter if using file storage and only set relationship if using database storage

--- models/state.py
+++ models/state.py
@@ -5,21 +5,25 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from models.city import City
+from os import getenv
+storage_type = getenv('HBNB_STORAGE_TYPE')
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    name = Column(String(128),
+    if storage_type == "db":
+        name = Column(String(128),
                   nullable=False)
-    cities = relationship("City", cascade="all, delete", backref="state")
+        cities = relationship("City", cascade="all, delete", backref="state")
 
-    @property
-    def cities(self):
-        """list of city instances"""
-        city_list = []
-        city_all = models.storage.all(City)
-        for city in city_all.values():
-            if city.state_id == self.id:
-                city_list.append(city)
-        return city_list
+    else:
+        @property
+        def cities(self):
+            """list of city instances"""
+            city_list = []
+            city_all = models.storage.all(City)
+            for city in city_all.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
